05a3f0ba92328a98af54ab734b020dab89ed0497
Tavi Mirishan <tavimirisan26@gmail.com>
2024-08-08 13:10:58 +0300

    almost done

--- Polynomial Calculator/src/main/java/GUI/CalculatorController.java
+++ Polynomial Calculator/src/main/java/GUI/CalculatorController.java
@@ -1,144 +1,242 @@
 package GUI;
 
+import BusinessLogic.Operations;
+import Model.Polynomial;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import jdk.incubator.vector.VectorOperators;
 
 public class CalculatorController {
 
     @FXML
-    TextField field1, field2, fieldr;
+    private TextField field1, field2, fieldr;
 
     @FXML
     private Button oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton, zeroButton;
     @FXML
     private Button plusButton, minusButton, slashButton, dinkusButton, circumfelxButton, dotButton, xButton, delButton;
     @FXML
-    private Button addButton, subButton, mulButton, divButon, derButton, intButton;
+    private Button addButton, subButton, mulButton, divButton, derButton, intButton;
+
+    private TextField activeField;
+    private int caretPosition1, caretPosition2;
+
+    private Operations operations;
+
 
     @FXML
     private void initialize() {
-        // Inițializările pot fi făcute aici dacă e necesar.
+        field1.setOnMouseClicked(e -> activeField = field1);
+        field2.setOnMouseClicked(e -> activeField = field2);
+        activeField = field1;
+
+        caretPosition1 = 0;
+        caretPosition2 = 0;
+
+        operations = new Operations();
+
     }
 
-    @FXML
-    private void oneClick() {
-        // Codul pentru acțiunea butonului 1
+    private void insertText(String text)
+    {
+        if (activeField != null)
+        {
+
+            if(activeField == field1)
+            {
+                activeField.insertText(caretPosition1, text);
+
+                caretPosition1 = caretPosition1 + text.length();
+                activeField.positionCaret(caretPosition1);
+
+            }
+            else if(activeField == field2)
+            {
+                activeField.insertText(caretPosition2, text);
+
+                caretPosition2 = caretPosition2 + text.length();
+                activeField.positionCaret(caretPosition2);
+
+            }
+
+
+        }
     }
 
-    @FXML
-    private void twoClick() {
-        // Codul pentru acțiunea butonului 2
-    }
+
+
+
 
     @FXML
-    private void threeClick() {
-        // Codul pentru acțiunea butonului 3
-    }
+    private void oneClick() { insertText("1"); }
 
     @FXML
-    private void fourClick() {
-        // Codul pentru acțiunea butonului 4
-    }
+    private void twoClick() { insertText("2"); }
 
     @FXML
-    private void fiveClick() {
-        // Codul pentru acțiunea butonului 5
-    }
+    private void threeClick() { insertText("3"); }
 
     @FXML
-    private void sixClick() {
-        // Codul pentru acțiunea butonului 6
-    }
+    private void fourClick() { insertText("4"); }
 
     @FXML
-    private void sevenClick() {
-        // Codul pentru acțiunea butonului 7
-    }
+    private void fiveClick() { insertText("5"); }
 
     @FXML
-    private void eightClick() {
-        // Codul pentru acțiunea butonului 8
-    }
+    private void sixClick() { insertText("6"); }
 
     @FXML
-    private void nineClick() {
-        // Codul pentru acțiunea butonului 9
-    }
+    private void sevenClick() { insertText("7"); }
 
     @FXML
-    private void zeroClick() {
-        // Codul pentru acțiunea butonului 0
-    }
+    private void eightClick() { insertText("8"); }
 
     @FXML
-    private void plusClick() {
-        // Codul pentru acțiunea butonului +
-    }
+    private void nineClick() { insertText("9"); }
 
     @FXML
-    private void minusClick() {
-        // Codul pentru acțiunea butonului -
-    }
+    private void zeroClick() { insertText("0"); }
 
     @FXML
-    private void slashClick() {
-        // Codul pentru acțiunea butonului /
-    }
+    private void plusClick() { insertText("+"); }
 
     @FXML
-    private void dinkusClick() {
-        // Codul pentru acțiunea butonului *
-    }
+    private void minusClick() { insertText("-"); }
 
     @FXML
-    private void circumfelxClick() {
-        // Codul pentru acțiunea butonului ^
-    }
+    private void slashClick() { insertText("/"); }
 
     @FXML
-    private void dotClick() {
-        // Codul pentru acțiunea butonului .
-    }
+    private void dinkusClick() { insertText("*"); }
 
     @FXML
-    private void xClick() {
-        // Codul pentru acțiunea butonului x
-    }
+    private void circumfelxClick() { insertText("^"); }
+
+    @FXML
+    private void dotClick() { insertText("."); }
+
+    @FXML
+    private void xClick() { insertText("x"); }
 
     @FXML
     private void delClick() {
-        // Codul pentru acțiunea butonului del
+        if (activeField != null)
+        {
+
+            if(activeField == field1)
+            {
+                if (caretPosition1 > 0)
+                {
+                    activeField.positionCaret(caretPosition1 - 1);
+                    activeField.deleteText(caretPosition1- 1, caretPosition1);
+                    caretPosition1--;
+
+                }
+
+            }
+            else if(activeField == field2)
+            {
+                if (caretPosition2 > 0)
+                {
+                    activeField.positionCaret(caretPosition2 - 1);
+                    activeField.deleteText(caretPosition2- 1, caretPosition2);
+                    caretPosition2--;
+
+
+                }
+            }
+        }
+
     }
 
+
     @FXML
-    private void addClick() {
-        // Codul pentru acțiunea butonului ADD
+    private void addClick()
+    {
+        String polynomialText1 = field1.getText();
+        String polynomialText2 = field2.getText();
+
+        //creare polinoame din stringuri
+        Polynomial polynomial1 = new Polynomial(polynomialText1);
+        Polynomial polynomial2 = new Polynomial(polynomialText2);
+
+        //efectuare operatii pe polinoame
+        Polynomial resultPolynomial = operations.add(polynomial1, polynomial2);
+
+        //afisare rezultat
+        fieldr.setText(resultPolynomial.toString());
+
     }
 
     @FXML
     private void subClick() {
-        // Codul pentru acțiunea butonului SUB
+
+        String polynomialText1 = field1.getText();
+        String polynomialText2 = field2.getText();
+
+        //creare polinoame din stringuri
+        Polynomial polynomial1 = new Polynomial(polynomialText1);
+        Polynomial polynomial2 = new Polynomial(polynomialText2);
+
+        //efectuare operatii pe polinoame
+        Polynomial resultPolynomial = operations.subtract(polynomial1, polynomial2);
+
+        //afisare rezultat
+        fieldr.setText(resultPolynomial.toString());
+
     }
 
     @FXML
     private void mulClick() {
-        // Codul pentru acțiunea butonului MUL
+
+
+        String polynomialText1 = field1.getText();
+        String polynomialText2 = field2.getText();
+
+        //creare polinoame din stringuri
+        Polynomial polynomial1 = new Polynomial(polynomialText1);
+        Polynomial polynomial2 = new Polynomial(polynomialText2);
+
+        //efectuare operatii pe polinoame
+        Polynomial resultPolynomial = operations.multiply(polynomial1, polynomial2);
+
+        //afisare rezultat
+        fieldr.setText(resultPolynomial.toString());
     }
 
     @FXML
     private void divClick() {
-        // Codul pentru acțiunea butonului DIV
+        // Cod pentru acțiunea butonului DIV
     }
 
     @FXML
     private void derClick() {
-        // Codul pentru acțiunea butonului DER
+
+        String polynomialText1 = field1.getText();
+
+        //creare polinoame din stringuri
+        Polynomial polynomial1 = new Polynomial(polynomialText1);
+
+        //efectuare operatii pe polinoame
+        Polynomial resultPolynomial = operations.derivate(polynomial1);
+
+        //afisare rezultat
+        fieldr.setText(resultPolynomial.toString());
     }
 
     @FXML
     private void intClick() {
-        // Codul pentru acțiunea butonului INT
+
+        String polynomialText1 = field1.getText();
+
+        //creare polinoame din stringuri
+        Polynomial polynomial1 = new Polynomial(polynomialText1);
+
+        //efectuare operatii pe polinoame
+        Polynomial resultPolynomial = operations.integrate(polynomial1);
+
+        //afisare rezultat
+        fieldr.setText(resultPolynomial.toString());
     }
 }
--- Polynomial Calculator/src/main/java/module-info.java
+++ Polynomial Calculator/src/main/java/module-info.java
@@ -3,8 +3,11 @@ module com.example.polynomialcalculator {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires jdk.incubator.vector;
 
     opens com.example.polynomialcalculator to javafx.fxml;
+    opens GUI to javafx.fxml;
+
     exports com.example.polynomialcalculator;
     exports GUI;
     exports BusinessLogic;
